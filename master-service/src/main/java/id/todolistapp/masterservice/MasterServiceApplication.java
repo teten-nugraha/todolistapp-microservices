f25afc3d06ce99316ad2a59140d90093799e52d2
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-02 16:59:12 +0700

    add openapi

--- master-service/src/main/java/id/todolistapp/masterservice/MasterServiceApplication.java
+++ master-service/src/main/java/id/todolistapp/masterservice/MasterServiceApplication.java
@@ -1,15 +1,37 @@
 package id.todolistapp.masterservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MasterServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MasterServiceApplication.class, args);
-	}
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application" +
+            "-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Master Service API")
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterServiceApplication.class, args);
+    }
 
 }
