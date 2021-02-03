76665a2b797826221447a84fa3a855efaaa3512a
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-03 12:01:36 +0700

    add monitor service

---
+++ monitor-service/src/test/java/id/taskapp/monitorservice/MonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.taskapp.monitorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
